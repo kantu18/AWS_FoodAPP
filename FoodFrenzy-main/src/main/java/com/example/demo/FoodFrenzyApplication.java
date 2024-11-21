f4cf3cc72b599a434e5c09c4afa74c15e57cc057
kantu18 <snehasisdutta21@gmail.com>
2024-11-21 16:24:11 +0530

    Pushing the code in Git

---
+++ FoodFrenzy-main/src/main/java/com/example/demo/FoodFrenzyApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.example.demo")
+public class FoodFrenzyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodFrenzyApplication.class, args);
+		
+		
+	}
+
+}
