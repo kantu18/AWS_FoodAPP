3da78139dada1c1b155fe4be6a00cc2f69a27216
Snehasis dutta <41549553+kantu18@users.noreply.github.com>
2024-11-21 20:52:49 +0530

    Update fix_privileges.sh

--- scripts/fix_privileges.sh
+++ scripts/fix_privileges.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-chmod +x /home/ec2-user/server/* jar
-chmod +x /home/ec2-user/server/server_start.sh
-chmod +x /home/ec2-user/server/server_stop.sh
+chmod +x /home/ec2-user/* jar
+chmod +x /home/ec2-user/server_start.sh
+chmod +x /home/ec2-user/server_stop.sh
