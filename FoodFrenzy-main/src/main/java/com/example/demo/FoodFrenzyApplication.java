0acdd48d77a42fe3b24c58ba9d5949eff12acf49
Snehasis dutta <41549553+kantu18@users.noreply.github.com>
2024-11-26 19:23:35 +0530

    Update FoodFrenzyApplication.java

--- FoodFrenzy-main/src/main/java/com/example/demo/FoodFrenzyApplication.java
+++ FoodFrenzy-main/src/main/java/com/example/demo/FoodFrenzyApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.example.demo")
 public class FoodFrenzyApplication {
 
 	public static void main(String[] args) {
